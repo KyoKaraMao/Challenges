3a27e91ca6fceb4934d0aca54e17fae8cb4a67a0
KyoKaraMao <69955181+KyoKaraMao@users.noreply.github.com>
2021-04-18 21:10:40 +0200

    so einfach gehts auch

--- src/de/p100/challanges/challanges.py
+++ src/de/p100/challanges/challanges.py
@@ -24,17 +24,12 @@ def min_operations(x, y):
                 calc = calc - 1
                 result.append(1)
                 operations += 1
-    try:
-        if(len(result) < len(best_result)):
-            best_result = result
-    except:
-        best_result = result
 
     end_result = ""
-    for each in best_result:
+    for each in result:
         end_result = end_result + "("
     end_result = end_result + format(x)
-    for each in best_result:
+    for each in result:
         if each == 1:
             end_result = end_result + " - 1)"
         if each == 2:
@@ -45,5 +40,5 @@ def min_operations(x, y):
     return end_result
 
 
-print(min_operations(6, 20))
+print(min_operations(2, 11))
 # (((6 - 1) * 2) * 2) = 20 : 3 operations needed only
