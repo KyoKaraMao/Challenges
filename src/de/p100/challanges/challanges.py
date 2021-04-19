89c503eb8f7c4fda4637af1d7b8dc6fd5fdfc2c2
KyoKaraMao <69955181+KyoKaraMao@users.noreply.github.com>
2021-04-19 13:49:32 +0200

    so einfach gehts auch

--- src/de/p100/challanges/challanges.py
+++ src/de/p100/challanges/challanges.py
@@ -3,44 +3,51 @@ def min_operations(x, y):
     # substract by 1
     calc = x
     result = list()
-    operations = 0
-    #Gerade
     if (y > x) and ((y%2)==0):
         while calc != y:
             if (y % calc) == 0:
                 calc = calc * 2
                 result.append(2)
-                operations += 1
             else:
                 calc = calc - 1
                 result.append(1)
-                operations += 1
-    #Ungerade
     else:
         while calc != y:
             if (((y+1) % calc) == 0) and (calc<y):
                 calc = calc * 2
                 result.append(2)
-                operations += 1
             else:
                 calc = calc - 1
                 result.append(1)
-                operations += 1
+
+    best_result = result
+
+    result = list()
+    while calc != x:
+        if ((calc %2) == 0) and ((calc/2) >= x):
+            calc = calc / 2
+            result.append(2)
+        else:
+            calc = calc + 1
+            result.append(1)
+
+    if(len(result) < len(best_result)):
+        best_result = result
 
     end_result = ""
-    for each in result:
+    for each in best_result:
         end_result = end_result + "("
     end_result = end_result + format(x)
-    for each in result:
+    for each in best_result:
         if each == 1:
             end_result = end_result + " - 1)"
         if each == 2:
             end_result = end_result + " * 2)"
 
-    end_result = end_result + "= {} : {} operations needed only".format(y,operations)
+    end_result = end_result + "= {} : {} operations needed only".format(y,len(best_result))
 
     return end_result
 
 
-print(min_operations(2, 11))
+print(min_operations(6, 20))
 # (((6 - 1) * 2) * 2) = 20 : 3 operations needed only
