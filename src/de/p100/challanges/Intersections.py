6db2611361bce3edf16b2ca418cde0108c138106
laura-str <laura.struempler@gmail.com>
2021-04-21 10:28:03 +0200

    Intersections mit einfacher Ausgabe

--- src/de/p100/challanges/Intersections.py
+++ src/de/p100/challanges/Intersections.py
@@ -1,7 +1,16 @@
-def intersection(l1, l2, l3):
-    a = [x for x in l2 if x in l1]
-    b = [x for x in l3 if x in a]
+def intersection(list1, list2, list3):
+    """
+    :lists format: [1, 2, 3, 4, 5, ...]
+    :param list1: list of numbers
+    :param list2: list of numbers
+    :param list3: list of numbers
+    :return: only numbers that are present in all three lists
+    """
+    a = [x for x in list2 if x in list1]
+    b = [x for x in list3 if x in a]
     return b
 
 
 print(intersection([1, 2, 3, 4], [2, 4, 6, 8], [3, 4, 5]))
+
+help(intersection)
