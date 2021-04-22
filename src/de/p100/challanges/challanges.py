da15b1974ee5dbfce583bcecac29ff4299554928
KyoKaraMao <69955181+KyoKaraMao@users.noreply.github.com>
2021-04-22 22:02:22 +0200

    Kinda cheat aber im richtigen Ausgabe format.

--- src/de/p100/challanges/challanges.py
+++ src/de/p100/challanges/challanges.py
@@ -1,52 +1,25 @@
+import timeit
+
 def min_operations(x, y):
     # multiply by 2
     # substract by 1
-    calc = x
-    max_operation = 300
-    result = list()
-    if (y > x) and ((y%2)==0):
-        while calc != y:
-            if (y % calc) == 0:
-                calc = calc * 2
-                result.append(2)
-            else:
-                calc = calc - 1
-                result.append(1)
-            if len(result)>max_operation:
-                result = list()
-                break
-    else:
-        while calc != y:
-            if (((y+1) % calc) == 0) and (calc<y):
-                calc = calc * 2
-                result.append(2)
-            else:
-                calc = calc - 1
-                result.append(1)
-            if len(result)>max_operation:
-                result = list()
-                break
-
-    best_result = result
+    calc = y
 
     result = list()
     while calc != x:
-        if ((calc %2) == 0) and ((calc/2) >= x):
+        if calc < x:
+            result_end = [1] * int(x - calc)
+            result.extend(result_end)
+            break
+        if ((calc %2) == 0):
             calc = calc / 2
             result.append(2)
         else:
             calc = calc + 1
             result.append(1)
-        if len(result)>max_operation:
-            result = list()
-            break
 
-    if(len(result) < len(best_result)):
-        result.reverse()
-        best_result = result
+    best_result = result
 
-    if len(best_result) == 0:
-        return "Mehr als {} operationen nötig".format(max_operation)
     end_result = ""
     for each in best_result:
         end_result = end_result + "("
@@ -61,6 +34,10 @@ def min_operations(x, y):
 
     return end_result
 
+start = timeit.default_timer()
 
-print(min_operations(1, 9999))
+print(min_operations(42357, 87877346))
 # (((6 - 1) * 2) * 2) = 20 : 3 operations needed only
+stop = timeit.default_timer()
+print('Time: ', stop - start)
+print()
