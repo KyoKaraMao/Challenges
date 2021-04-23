f53248027fadf90a09b414744075e927f2b86cae
KyoKaraMao <69955181+KyoKaraMao@users.noreply.github.com>
2021-04-23 08:32:48 +0200

    new Stile

--- src/de/p100/challanges/Intersections.py
+++ src/de/p100/challanges/Intersections.py
@@ -1,16 +1,43 @@
-def intersection(list1, list2, list3):
+import timeit
+
+# starting the timer
+start = timeit.default_timer()
+print()
+
+def intersection(list1, list2, list3, keep_duplicates):
     """
     :lists format: [1, 2, 3, 4, 5, ...]
     :param list1: list of numbers
     :param list2: list of numbers
     :param list3: list of numbers
+    :param keep_duplicates: True or Falls
     :return: only numbers that are present in all three lists
     """
-    a = [x for x in list2 if x in list1]
-    b = [x for x in list3 if x in a]
-    return b
 
+    def keep(list):
+        dups = []
+        for each in list:
+            if each in list:
+                dups.append(each)
+                list.remove(each)
+        return dups
 
-print(intersection([1, 2, 3, 4], [2, 4, 6, 8], [3, 4, 5]))
+    if keep_duplicates:
+        return list(set(list1) &
+                    set(list2) &
+                    set(list3)
+                    ) + \
+               list(set(keep(list1)) &
+                    set(keep(list2)) &
+                    set(keep(list3))
+                    )
 
-help(intersection)
+    else:
+        return set(list1) & set(list2) & set(list3)
+
+print(intersection([1, 2, 3, 4, 5, 5], [2, 4, 6, 8, 5, 5], [3, 4, 5, 5],True))
+
+# stopping the timer
+stop = timeit.default_timer()
+print('Time: ', stop - start)
+print()
--- src/de/p100/challanges/three_way_intersection.py
+++
@@ -1,7 +0,0 @@
-def intersection(l1, l2, l3):
-    return_list = list()
-    for each in l1:
-        if (each in l2) and (each in l3):
-            return_list.append(each)
-    return return_list
-print(intersection([1, 2, 3, 4, 5], [2, 4, 6, 8], [3, 4, 5]))
