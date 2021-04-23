315e664d4827edd68432805f5e19e2ed7e1d008a
ccneyo <quamidossantos@gmail.com>
2021-04-23 18:38:42 +0200

    ich bin kein toller hengst

--- src/de/p100/challanges/challanges.py
+++ src/de/p100/challanges/challanges.py
@@ -1,66 +1,65 @@
-def min_operations(x, y):
-    # multiply by 2
-    # substract by 1
-    calc = x
-    max_operation = 300
-    result = list()
-    if (y > x) and ((y%2)==0):
-        while calc != y:
-            if (y % calc) == 0:
-                calc = calc * 2
-                result.append(2)
-            else:
-                calc = calc - 1
-                result.append(1)
-            if len(result)>max_operation:
-                result = list()
-                break
-    else:
-        while calc != y:
-            if (((y+1) % calc) == 0) and (calc<y):
-                calc = calc * 2
-                result.append(2)
-            else:
-                calc = calc - 1
-                result.append(1)
-            if len(result)>max_operation:
-                result = list()
-                break
+# Python3 program to find minimum number of
+# steps needed to convert a number x into y
+# with two operations allowed :
+# (1) multiplication with 2
+# (2) subtraction with 1.
+import timeit
+import queue
 
-    best_result = result
+start = timeit.default_timer()
 
-    result = list()
-    while calc != x:
-        if ((calc %2) == 0) and ((calc/2) >= x):
-            calc = calc / 2
-            result.append(2)
-        else:
-            calc = calc + 1
-            result.append(1)
-        if len(result)>max_operation:
-            result = list()
-            break
-
-    if(len(result) < len(best_result)):
-        result.reverse()
-        best_result = result
-
-    if len(best_result) == 0:
-        return "Mehr als {} op_erationen nötig".format(max_operation)
-    end_result = ""
-    for each in best_result:
-        end_result = end_result + "("
-    end_result = end_result + format(x)
-    for each in best_result:
-        if each == 1:
-            end_result = end_result + " - 1)"
-        if each == 2:
-            end_result = end_result + " * 2)"
-
-    end_result = end_result + "= {} : {} operations needed only".format(y,len(best_result))
-
-    return end_result
+# A node of BFS traversal
+class node:
+    def __init__(self, val, level):
+        self.val = val
+        self.level = level
 
 
-print(min_operations(1, 9999))
-# (((6 - 1) * 2) * 2) = 20 : 3 operations needed only
+# Returns minimum number of operations
+# needed to convert x into y using BFS
+def minOperations(x, y):
+    # To keep track of visited numbers
+    # in BFS.
+    visit = set()
+
+    # Create a queue and enqueue x into it.
+    q = queue.Queue()
+    n = node(x, 0)
+    q.put(n)
+
+    # Do BFS starting from x
+    while (not q.empty()):
+
+        # Remove an item from queue
+        t = q.get()
+
+        # If the removed item is target
+        # number y, return its level
+        if (t.val == y):
+            return t.level
+
+        # Mark dequeued number as visited
+        visit.add(t.val)
+
+        # If we can reach y in one more step
+        if (t.val * 2 == y or t.val - 1 == y):
+            return t.level + 1
+
+        # Insert children of t if not visited
+        # already
+        if (t.val * 2 not in visit):
+            n.val = t.val * 2
+            n.level = t.level + 1
+            q.put(n)
+        if (t.val - 1 >= 0 and t.val - 1 not in visit):
+            n.val = t.val - 1
+            n.level = t.level + 1
+            q.put(n)
+
+stop = timeit.default_timer()
+print('Time: ', stop - start)
+# Driver code
+if __name__ == '__main__':
+    x = 4
+    y = 7
+    print(minOperations(x, y))
